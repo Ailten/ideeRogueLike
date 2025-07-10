40a8abb42d93d3914cd980831532077629329032
Ailten <faouzi26b@live.fr>
2025-07-10 17:31:04 +0200

    room algo edit

--- engine/classUtility/Run/CelType.cs
+++ engine/classUtility/Run/CelType.cs
@@ -2,14 +2,14 @@
 public enum CelType
 {
     //without any effect.
-    Cel = 0,
+    Cel,
 
 
     //door.
-    CelDoor_up = 1,
-    CelDoor_right = 2,
-    CelDoor_down = 3,
-    CelDoor_left = 4,
+    CelDoor_up,
+    CelDoor_right,
+    CelDoor_down,
+    CelDoor_left,
 
 
     Cel_NextStage,
--- engine/classUtility/Run/Room.cs
+++ engine/classUtility/Run/Room.cs
@@ -153,132 +153,57 @@ public class Room
         }
 
         //loop spreading, decreasing the amount of rng.
-        for(int r=0; r<rayonSpread; r++){
-	        for(int y=0; y<heightMax; y++){
-	        	for(int x=0; x<widthMax; x++){
-                    int distToCenterX = Math.Abs(x - midWidthMax);
-                    int distToCenterY = Math.Abs(y - midHeightMax);
-                    float dist = (float) Math.Sqrt(
-                        distToCenterX * distToCenterX +
-                        distToCenterY * distToCenterY
-                    );
-                    if(dist > r) //skip if not the current circle target at this loop.
-                        continue;
+	    for(int y=0; y<heightMax; y++){
+	    	for(int x=0; x<widthMax; x++){
+                int distToCenterX = Math.Abs(x - midWidthMax);
+                int distToCenterY = Math.Abs(y - midHeightMax);
+                float dist = (float) Math.Sqrt(
+                    Math.Pow(distToCenterX / rayonSpread, 2) +
+                    Math.Pow(distToCenterY / rayonSpread, 2)
+                );
+                if(dist > 1) //skip if not the current circle target at this loop.
+                    continue;
 
-                    float rayonI = Math.Clamp((float)r / (rayonSpread-1), 0f, 1f);
-                    int rngCeil = 999 - (int)(800 * rayonI); 
-                    int rngGet = rngSeed.Next(1000);
-                    bool isSafeCrossCel = (distToCenterX == 0 || distToCenterY == 0) || dist < 2.75f; //patern always cell valid.
-                    if (rngGet >= rngCeil && !isSafeCrossCel) //skip if cell rng say no.
-                        continue;
+                float rayonI = Math.Clamp(dist, 0f, 1f);
+                rayonI = rayonI / 2 + 0.5f; //adapt rayonI for randomise only the farest cells.
+                int rngCeil = 999 - (int)(800 * rayonI); 
+                int rngGet = rngSeed.Next(1000);
+                bool isSafeCrossCel = ( //patern always cell valid.
+                    (distToCenterX == 0 || distToCenterY == 0) ||
+                    (distToCenterX <= 2 && distToCenterY <= 1) //rectangle center safe.
+                );
+                if (rngGet >= rngCeil && !isSafeCrossCel) //skip if cell rng say no.
+                    continue;
 
-                    int countAdj = (
-                        ((y>0 && celsBool[y-1][x])? 1: 0) +
-                        ((y<(heightMax-1) && celsBool[y+1][x])? 1: 0) +
-                        ((x>0 && celsBool[y][x-1])? 1: 0) +
-                        ((x<(widthMax-1) && celsBool[y][x+1])? 1: 0)
-                    );
-                    if(countAdj == 0 && dist != 0) //skip if cell is not adjacent to another valid.
-                        continue;
-
-                    celsBool[y][x] = true;
-	        	}
-	        }
-        }
+                celsBool[y][x] = true;
+	    	}
+	    }
 
         //get door for other room.
-        Dictionary<int, Vector> posForDoor = new();
-        List<Vector> posInLine = new();
-
-        for(int d=0; d<4; d++){ //loop on 4 direction.
-
-            bool isNeedThisDoor = ( //skip this direction if no need door for it.
-                (d==0)? isARoomTop: //up.
-                (d==1)? isARoomRight: //right.
-                (d==2)? isARoomDown: //down.
-                isARoomLeft //left.
-            );
-            if(!isNeedThisDoor)
-                continue;
-
-            for(int y=0; y<heightMax; y++){ //find door to room up. --->
-
-                posInLine = new();
-
-            	for(int x=0; x<widthMax; x++){
-
-                    int xcast = ( //remap x and y for scroll order.
-                        (d==0)? x: //int x=0; x<width; x++
-                        (d==1)? (heightMax-1)-y: //int y=0; y<height; y++
-                        (d==2)? (widthMax-1)-x: //x=width-1; x>=0; x--
-                        y //int y=height-1; y>=0; y--
-                    );
-                    int ycast = (
-                        (d==0)? y: //int y=0; y<height; y++
-                        (d==1)? x: //x=width-1; x>=0; x--
-                        (d==2)? (heightMax-1)-y: //int y=height-1; y>=0; y--
-                        (widthMax-1)-x //int x=0; x<width; x++
-                    );
-
-                    if(celsBool[ycast][xcast])
-                        posInLine.Add(new(xcast, ycast));
-                }
-                if(posInLine.Count == 0)
-                    continue;
-
-                for(int i=0; i<posInLine.Count; i++){ //pop of line if already assigned to another door.
-                    foreach(KeyValuePair<int, Vector> posForDoorKeyValue in posForDoor){
-                        if(posInLine[i].x == posForDoorKeyValue.Value.x && posInLine[i].y == posForDoorKeyValue.Value.y){
-                            posInLine.RemoveAt(i);
-                            i--;
-                            break;
-                        }
-                    }
-                }
-                if(posInLine.Count == 0)
-                    continue;
-
-                break; //break loop y, when find a pos for door at this direction.
-            }
-            if(posInLine.Count == 0)
-                continue;
-
-            posForDoor.Add( //add pos door up.
-                d, 
-                posInLine[posInLine.Count/2]
-            );
-        }
+        List<KeyValuePair<CelType, Vector>> posForDoor = new();
+        posForDoor.Add(new(CelType.CelDoor_up, new(midWidthMax, midHeightMax - rayonSpread)));
+        posForDoor.Add(new(CelType.CelDoor_right, new(midWidthMax + rayonSpread, midHeightMax)));
+        posForDoor.Add(new(CelType.CelDoor_down, new(midWidthMax, midHeightMax + rayonSpread)));
+        posForDoor.Add(new(CelType.CelDoor_left, new(midWidthMax - rayonSpread, midHeightMax)));
 
         //add monster if is a normal type room.
         List<Vector> posCelForMobSpawner = new();
-        _typeMobToSpawn = new();
-        if(roomType == RoomType.Room){
+        this._typeMobToSpawn = new();
+        if (roomType == RoomType.Room)
+        {
 
             int amountOfMobInRoom = rngSeed.Next(1, 5);
 
-            for(int i=0; i<amountOfMobInRoom; i++){
+            for (int i = 0; i < amountOfMobInRoom; i++)
+            {
 
-                Vector posForMobSpawner = new( //random pos in square (0~n).
-                    rngSeed.Next(-radiusSafe, radiusSafe+1), 
-                    rngSeed.Next(-radiusSafe, radiusSafe+1)
+                Vector posForMobSpawner = new( //random pos in rectangle safe.
+                    midWidthMax + rngSeed.Next(-2, 3),
+                    midHeightMax + rngSeed.Next(-1, 2)
                 );
 
-                if(Math.Abs(posForMobSpawner.x) + Math.Abs(posForMobSpawner.y) > radiusSafe){ //skip if random pos not in circle.
-                    i--;
-                    continue;
-                }
-
-                posForMobSpawner.x += midWidthMax; //replace pos att center of room.
-                posForMobSpawner.y += midHeightMax;
-
-                bool isRandomPosBusy = false; //skip if random pos is already used.
-                for(int j=0; j<posCelForMobSpawner.Count; j++){
-                    if(posCelForMobSpawner[j].x == posForMobSpawner.x && posCelForMobSpawner[j].y == posForMobSpawner.y){
-                        isRandomPosBusy = true;
-                        break;
-                    }
-                }
-                if(isRandomPosBusy){
+                if (posCelForMobSpawner.Contains(posForMobSpawner))
+                {
                     i--;
                     continue;
                 }
@@ -287,19 +212,24 @@ public class Room
 
             }
 
-            for(int i=0; i<posCelForMobSpawner.Count; i++){ //generate random type mob for the room.
+            for (int i = 0; i < posCelForMobSpawner.Count; i++) //generate random type mob for the room.
+            {
                 typeMobToSpawn.Add(CharacterMob.generateRandomMobType(this, stage, false));
             }
 
-        }else if(roomType == RoomType.Room_Boss){ // spawner for boss.
+        }
+        else if (roomType == RoomType.Room_Boss) // spawner for boss.
+        {
 
             posCelForMobSpawner.Add(new Vector(midWidthMax, midHeightMax));
             typeMobToSpawn.Add(CharacterMob.generateRandomMobType(this, stage, true));
 
         }
 
+        //TODO: else if for other type special room.
+
         //cast celsBool into cels.
-        cels = new();
+        this.cels = new();
         for(int y=0; y<heightMax; y++){
             cels.Add(new());
         	for(int x=0; x<widthMax; x++){
@@ -307,30 +237,30 @@ public class Room
                     continue;
                 }
 
-                int indexCelType = 0;
-                foreach(KeyValuePair<int, Vector> posForDoorKeyValue in posForDoor){ //set type for door.
+                CelType celType = CelType.Cel;
+                foreach(KeyValuePair<CelType, Vector> posForDoorKeyValue in posForDoor){ //set type for door.
                     if(posForDoorKeyValue.Value.x == x && posForDoorKeyValue.Value.y == y){
-                        indexCelType = posForDoorKeyValue.Key + 1;
+                        celType = posForDoorKeyValue.Key;
                     }
                 }
 
-                if(indexCelType == 0){
+                if(celType == CelType.Cel){
                     foreach(Vector posForMobSpawner in posCelForMobSpawner){ //set type for mob spawner.
                         if(posForMobSpawner.x == x && posForMobSpawner.y == y){
-                            indexCelType = (int)CelType.Cel_MobSpawner;
+                            celType = CelType.Cel_MobSpawner;
                         }
                     }
                 }
 
-                if(indexCelType == 0 && roomType==RoomType.Room_Center && x==midWidthMax && y==midHeightMax){
-                    indexCelType = (int)CelType.Cel_CenterRoom; //print center room for spawn stage cel.
+                if(celType == CelType.Cel && roomType==RoomType.Room_Center && x==midWidthMax && y==midHeightMax){
+                    celType = CelType.Cel_CenterRoom; //print center room for spawn stage cel.
                 }
 
                 //instancie cel.
                 cels[cels.Count-1].Add(
                     x,
                     new Cel(
-                        (CelType)indexCelType,
+                        celType,
                         new(x, y)
                     )
                 );
