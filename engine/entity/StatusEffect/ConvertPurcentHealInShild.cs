6879f5322d918091e711c5fcca93be16eff330ce
Ailten <faouzi26b@live.fr>
2025-08-28 17:25:48 +0200

    SE fonctionnel

--- engine/entity/StatusEffect/ConvertPurcentHealInShild.cs
+++ engine/entity/StatusEffect/ConvertPurcentHealInShild.cs
@@ -8,4 +8,33 @@ public class ConvertPurcentHealInShild : StatusEffect
     {
         this.purcentConvert = purcentConvert;
     }
+
+
+    public override string getDescription()
+    {
+        int purcent = (int)(this.purcentConvert * 100);
+        return (
+            $"- {this.getName()} :\n" +
+            $"converti {purcent}% des soin realise en bouclier.\n" +
+            this.getDescriptionTurn()
+        );
+    }
+    protected override string getName()
+    {
+        return $"Convertion soin bouclier";
+    }
+    public override bool isAMalus()
+    {
+        return this.purcentConvert < 0f;
+    }
+
+
+    public override void eventWhenMakeAHeal(ref Character target, ref int healIncrement, ref PackageRefCard? refCard)
+    {
+        int convertAmount = (int)(healIncrement * this.purcentConvert);
+
+        healIncrement -= convertAmount;
+
+        this.getCharacterWhoHasEffect.giveShild(target, convertAmount, refCard);
+    }
 }
