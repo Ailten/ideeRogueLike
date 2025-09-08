70aca51ddc3ef16c166355514bb36e0644e3c309
Ailten <faouzi26b@live.fr>
2025-09-08 12:13:47 +0200

    add armor mob

---
+++ engine/entity/Character/CharacterMob/CharacterArmor.cs
@@ -0,0 +1,56 @@
+
+public class CharacterArmor : CharacterMob
+{
+    public CharacterArmor(Vector posIndexCel) : base(SpriteType.Character_Armor, posIndexCel)
+    {
+        //IA logic.
+        this.logicState.Add(LogicState.shildAlly);
+        this.logicState.Add(LogicState.chase);
+        this.logicState.Add(LogicState.shildAlly);
+        this.logicState.Add(LogicState.firstHit);
+        this.logicState.Add(LogicState.skipTurn);
+
+        //stats.
+        this.MPmax = 2;
+        this.MP = MPmax;
+        this.APmax = 3;
+        this.AP = APmax;
+        this.HPmax = 20;
+        this.HP = HPmax;
+
+        //gold can be looted.
+        this.PO = RandomManager.rng.Next(3, 7);
+
+        // effects.
+        this.AddStatusEffect(new ShildMultBoostColor(this.idEntity, -1, -1, CardColor.Blue, 1.1f)); // imune to blue damage.
+        this.AddStatusEffect(new ShildMultBoostColor(this.idEntity, -1, -1, CardColor.Green, 0.9f)); // imune to blue damage.
+        
+        //set deck.
+        this.deck.pickCountByTurn = 2;
+        this.deck.addCardToDeck(
+            new Card(
+                cardIllu: SpriteType.CardImg_Sword,
+                cardColor: CardColor.Green,
+                cardEdition: CardEdition.Default,
+                APCost: 1,
+                distanceToUse: new(1, 1),
+                effect: new KeyValuePair<EffectCard, int>(EffectCard.Hit, 5)
+            ),
+            amountOfCardAdd: 1,
+            isSameColor: false
+        );
+        this.deck.addCardToDeck(
+            new Card(
+                cardIllu: SpriteType.CardImg_Flag,
+                cardColor: CardColor.Green,
+                cardEdition: CardEdition.Default,
+                APCost: 2,
+                distanceToUse: new(1, 2),
+                effect: new KeyValuePair<EffectCard, int>(EffectCard.Shild, 1),
+                isInLine: false
+            ),
+            amountOfCardAdd: 1,
+            isSameColor: false
+        );
+    }
+}
